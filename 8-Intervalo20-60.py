4583f9738d8aa446cfe3d655741bdf87ad9b91cc
Calos Cen <carlosv1233@hotmail.com>
2017-03-23 16:42:30 -0600

    Intervalo de n numeros entre 20 y 60

---
+++ 8-Intervalo20-60.py
@@ -0,0 +1,9 @@
+#generar un intervalo de n numeros entre 20 y 60
+#forma de llamar al metodo
+#"intervN (30)" (es un ejemplo).
+def intervaloN (x):
+        cont=1
+        while cont<=x:
+                if cont>=20 and cont<=60:
+                        print cont
+                cont+=1
