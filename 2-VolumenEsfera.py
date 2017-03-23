d8aac6b308c762e735dc8face0caf2c53fb52c36
Calos Cen <carlosv1233@hotmail.com>
2017-03-23 16:34:56 -0600

    Cacula el volumen de una esfera

---
+++ 2-VolumenEsfera.py
@@ -0,0 +1,6 @@
+#permite obtener el volumen de una esfera.
+#calcular el volumen por radio
+#"volumen (20)" (Es un ejemplo).
+def volumen(radio):
+	vol=(1.33*3.1416)*(float(radio)**3)
+	print "volumen es: ", vol
