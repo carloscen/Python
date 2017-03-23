c4f576e5b828c9fcbd4278619993b5f3330a2800
Calos Cen <carlosv1233@hotmail.com>
2017-03-23 16:43:27 -0600

    Solicita dos argumentos de tipo Double para calcular la hipotenusa

---
+++ 9-Hipotenusa.py
@@ -0,0 +1,7 @@
+#solicite dos argumentos de tipo Double para calcular la hipotenusa 
+#de un triángulo rectángulo y retorne un valor de tipo Double.
+#forma de llamar al metodo
+#"hipotenusa (2,4)" (es un ejemplo).
+import math
+def hipotenusa (x, y):
+        return math.sqrt(x**2 + y**2)
