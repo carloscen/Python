1b822eb4cfe4550c8b4455c0076a544a35c1eb26
Calos Cen <carlosv1233@hotmail.com>
2017-03-23 19:13:02 -0600

    Generar 10 impares apartir del 13

---
+++ 3-Impares-13-33.py
@@ -0,0 +1,7 @@
+#permite obtener intervalo de 10 numeros impares apartir del 13
+#forma de llamar al metodo
+#"intervalo" (es un ejemplo).
+def intervalo (impar=13):
+	while impar <=32:
+		print "Impar: ",str(impar)
+		impar+=2
